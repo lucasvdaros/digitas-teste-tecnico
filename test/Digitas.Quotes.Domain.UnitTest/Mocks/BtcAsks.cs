b0853fe2f5f2a5abc451d8f7b431a0e559e4f574
Lucas Daros <lucas.daros@invillia.com>
2024-01-31 23:16:33 -0300

    test: exemplos de testes unitários

---
+++ test/Digitas.Quotes.Domain.UnitTest/Mocks/BtcAsks.cs
@@ -0,0 +1,35 @@
+﻿using Digitas.Quotes.Domain.Entities;
+using Digitas.Quotes.Domain.ValueObjects;
+
+namespace Digitas.Quotes.Domain.UnitTest.Mocks;
+
+public class BtcAsks
+{
+    protected BtcAsks() { }
+
+    public static List<BtcAsk> BuildBtcAskList()
+    {
+        return new List<BtcAsk>
+        {
+            new BtcAsk() { Amount = 0.72000000M, BtcAskId = 3132, Microtimestamp = 1706663869812333, UsdValue = 42851 },
+            new BtcAsk() { Amount = 0.26000000M, BtcAskId = 3134, Microtimestamp = 1706663869812333, UsdValue = 42851 },
+            new BtcAsk() { Amount = 0.26000000M, BtcAskId = 3133, Microtimestamp = 1706663869812333, UsdValue = 42856 },
+            new BtcAsk() { Amount = 1.20000000M, BtcAskId = 3135, Microtimestamp = 1706663869812333, UsdValue = 42856 },
+            new BtcAsk() { Amount = 0.76000000M, BtcAskId = 3136, Microtimestamp = 1706663869812333, UsdValue = 42857 },
+            new BtcAsk() { Amount = 0.01000000M, BtcAskId = 3137, Microtimestamp = 1706663869812333, UsdValue = 42859 },
+        };
+    }
+
+    public static List<Quote> BuildQuoteList()
+    {
+        return new List<Quote>
+        {
+            new Quote(0.72000000M, 42851),
+            new Quote(0.26000000M, 42851),
+            new Quote(0.26000000M, 42856),
+            new Quote(1.20000000M, 42856),
+            new Quote(0.76000000M, 42857),
+            new Quote(0.01000000M, 42859)
+        };
+    }
+}
--- test/Digitas.Quotes.Domain.UnitTest/Services/BtcOperationServiceTest.cs
+++ test/Digitas.Quotes.Domain.UnitTest/Services/BtcOperationServiceTest.cs
@@ -1,6 +1,55 @@
-﻿namespace Digitas.Quotes.Domain.UnitTest.Services;
+﻿using Digitas.Quotes.Domain.Enums;
+using Digitas.Quotes.Domain.Interfaces.Repositories;
+using Digitas.Quotes.Domain.Interfaces.Services;
+using Digitas.Quotes.Domain.Services;
+using Digitas.Quotes.Domain.UnitTest.Mocks;
+using Digitas.Quotes.Domain.ValueObjects;
+using FluentAssertions;
+using Moq;
+
+namespace Digitas.Quotes.Domain.UnitTest.Services;
 
 public class BtcOperationServiceTest
 {
+    private readonly Mock<IBtcAskRepository> mockBtcAskRepository;
+    private readonly Mock<IBtcBidRepository> mockBtcBidRepository;
+    private readonly Mock<IQuoteService> mockQuoteRepository;
+
+    public BtcOperationServiceTest()
+    {
+        mockBtcAskRepository = new Mock<IBtcAskRepository>();
+        mockBtcBidRepository = new Mock<IBtcBidRepository>();
+        mockQuoteRepository = new Mock<IQuoteService>();
+    }
+
+    [Theory]
+    [InlineData(OperationChoice.Buy, 0.05)]
+    public async Task BtcOperationService_CalculateBtcBuyOperation_Given_Operation_Amount_When_Valid_ShouldReturnSimulation(OperationChoice operation, decimal amount)
+    {
+        //Arrange
+        mockBtcAskRepository.Setup(btc => btc.GetLastUpdatedQuoutes()).ReturnsAsync(BtcAsks.BuildBtcAskList());
+        mockQuoteRepository.Setup(btc => btc.SelectQuoutesByAmountRequested(It.IsAny<IEnumerable<ValueObjects.Quote>>(), amount)).Returns(BtcAsks.BuildQuoteList());
+        mockQuoteRepository.Setup(btc => btc.CalculateFinalPriceOffer(It.IsAny<List<Quote>>(), amount)).Returns(1);
+
+        var service = new BtcOperationService(mockQuoteRepository.Object, mockBtcAskRepository.Object, mockBtcBidRepository.Object);
+
+        //Act
+        var simulation = await service.CalculateBtcBuyOperation(operation, amount);
+
+        //Assert
+        simulation.Operation.Should().Be(operation);
+        simulation.Quotes.Should().NotBeNullOrEmpty();
+        simulation.Coin.Should().Be(Coin.BTC);
+        simulation.hashIdentificacao.Should().NotBeNullOrEmpty();
+        simulation.RequestAmount.Should().Be(amount);
+        simulation.Result.Should().Be(1);
+    }
+
+    [Theory]
+    [InlineData(OperationChoice.Sell, 0.05)]
+    public async Task BtcOperationService_CalculateBtcSellOperation_Given_Operation_Amount_When_Valid_ShouldReturnSimulation(OperationChoice operation, decimal amount)
+    {
+
+    }
 
 }
